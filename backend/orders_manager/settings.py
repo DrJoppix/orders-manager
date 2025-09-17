f04b34527a9c3518e277278c88eba1581fa01f1c
Giovanni Vaina <vainagiovanni96@gmail.com>
2025-09-17 23:32:36 +0200

    aggiunta app django per gestione cors e resa disponibile a tutte le origins

--- backend/orders_manager/settings.py
+++ backend/orders_manager/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'django_filters',
-    'orders',
+    "rest_framework",
+    "django_filters",
+    "orders",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+## CORS Configuration
+# Permetto tutte le origini per facilitare lo sviluppo e il testing.
+CORS_ALLOW_ALL_ORIGINS = True
